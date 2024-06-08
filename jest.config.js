1062eb2f1bf65d261ca7eef997c31cb8296f0013
Aymen Bahmed <joewilliamdd31@gmail.com>
2024-06-08 09:34:08 +0100

    Ignore TS errors for Jest

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,15 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
+  transform: {
+    "^.+\\.test.ts?$": [
+      "ts-jest",
+      {
+        diagnostics: {
+          exclude: ["**"],
+        },
+      },
+    ],
+  },
+  transformIgnorePatterns: ["<rootDir>/node_modules/"],
 };
